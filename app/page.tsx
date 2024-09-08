ca933aaa9c11fe6265b56a3c117e9a2e41004358
Theo in Tech <theoroque95@gmail.com>
2024-09-08 21:22:56 +0800

    refactor: create a temporary landing page for solana breakpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,159 @@
+"use client";
+
 import { OutlineButton } from "@/components";
 import { PATH } from "@/lib/routes";
-import { Stack } from "@mui/material";
+import XIcon from "@mui/icons-material/X";
+import { Box, Stack, Typography, useTheme } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const theme = useTheme();
   return (
     <Stack
       sx={{
         height: "100%",
+        minHeight: "100vh",
         width: "100%",
         alignItems: "center",
         justifyContent: "center",
+        rowGap: theme.spacing(6),
+        marginTop: { xs: theme.spacing(6), md: 0 },
+        marginBottom: { xs: theme.spacing(12), md: 0 },
       }}
     >
-      <Link href={PATH.breakpoint}>
-        <OutlineButton
-          preset="warning"
-          sx={{ padding: "32px", boxShadow: "3px 5px 0px 3px #D4A017" }}
+      <Stack
+        sx={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            height: 240,
+            width: 240,
+            position: "relative",
+          }}
         >
           <Image
-            src={"/assets/events/solana-breakpoint-2024/logo-breakpoint.png"}
-            alt={"Breakpoint Logo"}
-            width={1561}
-            height={747}
+            src="/assets/logo/animated.gif"
+            alt="Animated Logo"
+            fill
             style={{
-              height: "100px",
-              width: "fit-content",
+              objectFit: "cover",
+              objectPosition: "center",
             }}
+            priority
           />
-        </OutlineButton>
-      </Link>
+        </Box>
+        <Typography
+          sx={{
+            ...theme.typography.base.xl,
+            color: theme.palette.neutral[0],
+            textAlign: "center",
+          }}
+        >
+          Mint the moment. UnBlink.
+        </Typography>
+      </Stack>
+
+      <Stack
+        rowGap={4}
+        alignItems={"center"}
+        justifyContent={"center"}
+        position={"relative"}
+      >
+        {/* <Link href={PATH.breakpoint}> */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: theme.spacing(-2),
+            right: theme.spacing(-2),
+            background: theme.palette.neutral[0],
+            borderRadius: theme.spacing(2),
+            color: theme.palette.neutral[80],
+            padding: theme.spacing(1, 2.5),
+            zIndex: 100,
+          }}
+        >
+          Soon!
+        </Box>
+        <Link href={"#"}>
+          <OutlineButton
+            preset="warning"
+            sx={{ padding: "32px", boxShadow: "3px 5px 0px 3px #D4A017" }}
+          >
+            <Image
+              src={"/assets/events/solana-breakpoint-2024/logo-breakpoint.png"}
+              alt={"Breakpoint Logo"}
+              width={1561}
+              height={747}
+              style={{
+                height: "100px",
+                width: "fit-content",
+              }}
+            />
+          </OutlineButton>
+        </Link>
+        <Link href={PATH.external.shareOnX}>
+          <Stack
+            flexDirection={"row"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            columnGap={1}
+            sx={{
+              cursor: "pointer",
+              ":hover": {
+                transform: "scale(1.02)",
+                transition: "transform 0.3s ease-in-out",
+                filter: "brightness(1.1)",
+              },
+            }}
+          >
+            <Typography
+              sx={{
+                ...theme.typography.base.xl,
+                color: theme.palette.warning.main,
+                textAlign: "center",
+              }}
+            >
+              Share #BreakpointBingo on
+            </Typography>
+            <XIcon
+              sx={{
+                fontSize: "28px",
+                padding: theme.spacing(0.75),
+                borderRadius: "50%",
+                background: "#000",
+                color: theme.palette.neutral[0],
+              }}
+            />
+          </Stack>
+        </Link>
+      </Stack>
+
+      <Typography
+        sx={{
+          ...theme.typography.base.xxl,
+          color: theme.palette.warning.main,
+          textAlign: "center",
+        }}
+      >
+        - - -
+      </Typography>
+
+      <Typography
+        sx={{
+          ...theme.typography.base.md,
+          color: theme.palette.neutral[20],
+          textAlign: "center",
+          padding: theme.spacing(0, 2),
+        }}
+      >
+        Capture life&apos;s authentic moments, mint them as unique NFTs on
+        Solana, and earn royalties through shared, unfiltered photographic
+        experiences.
+      </Typography>
     </Stack>
   );
 }
--- lib/routes.ts
+++ lib/routes.ts
@@ -5,6 +5,12 @@ export const PATH = {
   // Events
   breakpoint: "/events/breakpoint",
   breakpointScan: "/events/breakpoint/scan",
+
+  // External
+  external: {
+    shareOnX:
+      "https://x.com/intent/post?text=Don%27t+Blink%21+%F0%9F%91%80%0A%0A%40UnBlinkApp%0A%23MintTheMoment%0A%0A%40solana%0A%40SolanaConf%0A%23Breakpoint2024+%0A%23BreakpointBingo",
+  },
 };
 
 export const replacePathKeys = (

b4a429c4d2b6668acb17dbf4db1070f533127e4c
Theo in Tech <theoroque95@gmail.com>
2024-09-08 21:46:12 +0800

    refactor: update share link

--- lib/routes.ts
+++ lib/routes.ts
@@ -9,7 +9,7 @@ export const PATH = {
   // External
   external: {
     shareOnX:
-      "https://x.com/intent/post?text=Don%27t+Blink%21+%F0%9F%91%80%0A%0A%40UnBlinkApp%0A%23MintTheMoment%0A%0A%40solana%0A%40SolanaConf%0A%23Breakpoint2024+%0A%23BreakpointBingo",
+      "https://twitter.com/intent/post?text=Don%27t+Blink%21+%F0%9F%91%80%0A%0A%40UnBlinkApp%0A%23MintTheMoment%0A%0A%40solana%0A%40SolanaConf%0A%23Breakpoint2024+%0A%23BreakpointBingo",
     maibaStudio: "https://maiba.studio/",
   },
 };
